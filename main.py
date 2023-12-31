df2f288b8b1e241f4a0cb37f213189574ec57eb0
itsjacob <conyersjacob21@gmail.com>
2023-12-31 16:19:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame as pg
+
+window = pg.display.set_mode((300, 300))
+
+pg.display.set_caption("Average 10 level platformer")
+
+pg.display.flip()
+running = True
+while running:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+            pg.quit()
