59150e2f821d6450150eba254e3fd9373f34ce93
Fungaming51 <121271641+Fungaming51@users.noreply.github.com>
2024-01-01 15:11:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
+import pygame as pg
 
+pg.init()
+
+window = pg.display.set_mode((300, 300))
+
+pg.display.set_caption("Average 10 level platformer")
+
+pg.display.flip()
+running = True
+while running:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+            pg.quit()
